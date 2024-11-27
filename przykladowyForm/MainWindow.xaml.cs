f420d261ad6044cacd79380b7fb0d4e0607f5b82
barte28 <bartek.teuerle@gmail.com>
2024-11-27 13:04:45 +0100

    gotowe

--- przykladowyForm/MainWindow.xaml.cs
+++ przykladowyForm/MainWindow.xaml.cs
@@ -24,5 +24,35 @@ namespace przykladowyForm
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int obwod;
+            int bok;
+            if(int.TryParse(bok_textbox.Text, out bok))
+            {
+                MessageBox.Show(bok.ToString());
+                string figura = figura_ComboBox.Text;
+                if(figura == "Kwadrat")
+                {
+                    obwod = bok * 4;
+                    MessageBox.Show("Obwód kwadratu wynosi " + obwod.ToString());
+                }
+                else if(figura == "Trójkąt prostokątny")
+                {
+                    obwod = bok * 3;
+                    MessageBox.Show("Obwód trójkąta prostokątnego wynosi " + obwod.ToString());
+                }
+                else if(figura == "Pięciokąt")
+                {
+                    obwod = bok * 5;
+                    MessageBox.Show("Obwód pięciokąta wynosi " + bok.ToString());
+                }
+            }
+            else
+            {
+                MessageBox.Show("To nie jest liczba");
+            }
+        }
     }
 }
